0db708dee67591f78e0e78d03674341437c63ba0
dsyang <dsyang.tech@gmail.com>
2023-10-02 20:53:42 +0900

    연습문제 13-3

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -37,6 +37,18 @@ defmodule Issues.CLI do
   def process({ user, project, _count }) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
+  end
+
+  def sort_into_descending_order(list_of_issues) do
+    list_of_issues
+    |> Enum.sort(fn i1, i2 -> i1["created_at"] >= i2["created_at"] end)
+  end
+
+  def last(list, count) do
+    list
+    |> Enum.take(count)
+    |> Enum.reverse
   end
 
   def decode_response({:ok, body}), do: body
