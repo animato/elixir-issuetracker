1974fe10da963071ce7afa2bfe5cdf63e068a5d6
dsyang <dsyang.tech@gmail.com>
2023-10-03 21:16:02 +0900

    13.11

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -1,4 +1,5 @@
 defmodule Issues.CLI do
+  import Issues.TableFormatter, only: [ print_table_for_columns: 2]
   @default_count 4
   @moduledoc """
   명령줄 파싱을 수행한 뒤, 각종 함수를 호출해 깃허브 프로젝트의 최근 _n_개 이슈를 표 형식으로 만들어 출력한다.
@@ -34,10 +35,12 @@ defmodule Issues.CLI do
     System.halt(0)
   end
 
-  def process({ user, project, _count }) do
+  def process({ user, project, count }) do
     Issues.GithubIssues.fetch(user, project)
     |> decode_response()
     |> sort_into_descending_order()
+    |> last(count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def sort_into_descending_order(list_of_issues) do
